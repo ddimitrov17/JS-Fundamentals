596b5641c83d26b866159f0b96f2af8901539928
Dimitar Dimitrov <149900928+ddimitrov17@users.noreply.github.com>
2024-01-10 11:30:04 +0200

    Add files via upload

---
+++ Basic Syntax CS Loops/MultiplyByTwo.js
@@ -0,0 +1,7 @@
+function MultiplyByTwo (input) {
+    let num=Number(input);
+    let r=num*2;
+    console.log(r);
+}
+
+MultiplyByTwo("2")
---
+++ Basic Syntax CS Loops/numbersFromNtoOne.js
@@ -0,0 +1,8 @@
+function numbersFromNtoOne(n) {
+    while (n>=1) {
+        console.log(n);
+        n--;
+    }
+}
+
+numbersFromNtoOne(4)
