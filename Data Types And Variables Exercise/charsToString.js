2369955aa4e49e43e27c5c74971833ec4c1d2bbe
Dimitar Dimitrov <149900928+ddimitrov17@users.noreply.github.com>
2024-01-21 10:45:29 +0200

    Add files via upload

---
+++ Data Types And Variables Exercise/charsToString.js
@@ -0,0 +1,10 @@
+function charsToString(symbol1,symbol2,symbol3) {
+    let total=symbol1+symbol2+symbol3;
+    console.log(total);
+}
+
+charsToString('%',
+
+'2',
+
+'o')
---
+++ Data Types And Variables Exercise/reversedChars.js
@@ -0,0 +1,10 @@
+function reversedChars(char1,char2,char3) {
+    let total=`${char3} ${char2} ${char1}`;
+    console.log(total);
+}
+
+reversedChars('A',
+
+'B',
+
+'C')
